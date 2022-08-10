8e67def5325e9677e31545ad89ffd4b152f591d5
ALLTALE2027 <86816300+ALLTALE2027@users.noreply.github.com>
2022-08-11 00:50:41 +0530

    [Darshan]: Added UC-1 line length

---
+++ Line_Length/LineLength.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Length
+{
+    internal class LineLength
+    {
+        public void lineLength()
+        {
+            // --- UC-1 model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system,calculate its length --- //
+
+            // take two co-ordinate points(x,y) as input from user
+
+            Console.WriteLine("enter the cordinates for first point");
+            (float x1, float y1) = Program.Point();
+
+            Console.WriteLine("enter the cordinates for second point");
+            (float x2, float y2) = Program.Point();
+
+            //Console.WriteLine(" x1 " + x1+ " " + y1);
+            //Console.WriteLine(" x2 " + x2 + " " + y2);
+
+            //calculate length of line
+            double length = Program.length(x1, y1, x2, y2);
+
+            Console.WriteLine("Length of the line is : " + Math.Round(length, 3));
+
+
+        }
+    }
+}
---
+++ Line_Length/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Line_Length
+{
+    internal class Program
+    {
+        public static (float, float) Point()   // (DRY)  method to read co-ordinate points 
+        {
+            float x = float.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+            return (x, y);
+        }
+
+        public static double length(float x, float y, float xx, float yy)
+        {
+            double length = Math.Sqrt(Math.Pow((xx - x), 2) + Math.Pow((yy - y), 2));
+            return length;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program!!!");
+
+            //UC-1
+            LineLength Length = new LineLength();
+            Length.lineLength();
+
+
+        }
+    }
+}
